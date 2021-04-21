9897d2a170c61940143abd798942781727ea7fc9
HostageKillar <53386919+HostageKillar@users.noreply.github.com>
2021-04-21 20:40:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from discord.ext import commands
 from discord.utils import get
 import os
 
+intents = discord.Intents.default()
+intents.members = True
+
 app = commands.Bot(command_prefix='>>', intents=intents)
 
 
